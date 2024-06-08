a4a6159a988092f58810953e482a5d61825c1581
asheeshyadav1 <yasheesh1013@gmail.com>
2024-06-08 14:58:55 -0700

    first commit

---
+++ src/test/java/rectangle/homework/rectangle/RectangleApplicationTests.java
@@ -0,0 +1,13 @@
+package rectangle.homework.rectangle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RectangleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
